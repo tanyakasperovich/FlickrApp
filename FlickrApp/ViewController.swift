afe320206aa0afeac3718f9c3d878180869104e2
Tatsiana Kasperovich <tanyakasperovich@mail.ru>
2023-09-21 09:29:05 +0300

    Initial Commit

---
+++ FlickrApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrApp
+//
+//  Created by Татьяна Касперович on 21.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
